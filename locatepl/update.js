621e0d63774b4af8c5fb07fca975f8e1826ab1e1
LateralCircle83 <11099324+lateralcircle83@user.noreply.gitee.com>
2023-01-05 00:40:57 +0000

    update locatepl/update.js.
    Signed-off-by: LateralCircle83 <11099324+lateralcircle83@user.noreply.gitee.com>

--- locatepl/update.js
+++ locatepl/update.js
@@ -1,4 +1,4 @@
-network.httpGet('https://gitee.com/lateralcircle83/eval/raw/master/locatepl/date2.3.0/locatepl_main_debug2.3.1/locatepl_main_2.3.1.js', (st, str) => {
+network.httpGet('https://gitee.com/lateralcircle83/eval/raw/master/locatepl/date2.4.0/locatepl_main.js', (st, str) => {
     log(st)
     if (st==200) {
         logger.log('获取新版本成功')
@@ -7,7 +7,8 @@ network.httpGet('https://gitee.com/lateralcircle83/eval/raw/master/locatepl/date
         let fine = File.readFrom('.\\plugins\\locatepl\\conf.json')
         logger.log('更换版本号')
         let obj = JSON.parse(fine)
-        obj.version = '2.3.1'
+        obj.version = '2.4'
+        obj["use_direction_api"]=true
         File.writeTo('.\\plugins\\locatepl\\conf.json', JSON.stringify(obj))
         logger.log('版本号更换完成，启动中……')
         mc.runcmdEx("ll load ./plugins/locatepl/locatepl_main.js"); mc.runcmdEx("ll unload locatepl_first")
