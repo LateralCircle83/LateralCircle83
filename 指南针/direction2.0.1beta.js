9e5539372db381fbe1f96065f96ec614a28029f1
LateralCircle83 <11099324+lateralcircle83@user.noreply.gitee.com>
2023-07-10 03:46:24 +0000

    add js
    Signed-off-by: LateralCircle83 <11099324+lateralcircle83@user.noreply.gitee.com>

---
+++ 指南针/direction2.0.1beta.js
@@ -0,0 +1,393 @@
+/**⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⡂⠈⠀⣁⠀⢠⡟⠀⠀⠀⠀⠈⠀⠀⠀⠀⠀⠀⠀⠚⠁⠀⠀⠀⠀⠀⠀⠀⠨⠀⠀⠀⠀⣰⡿⠛⠉⠛⠳⢶⣄⠁⢀⠀⠀⠀⠀⠀⠀⠀⢠⡾⠟⠡⡌⢈⠄⠙⠒⣤⡀⢠⠀⠁⠀⢀⡜⠁⠀⠀⠀⢨⢁⠀⠂⠀⠀⠀⠀⢸⡈⡇⠶⡄⠁⠁⠁⠉⠙⢦⣄⠀⡀⡀⠀⠀⠀⠀⠀⠀
+⠀⠐⢰⠂⣠⣷⠃⠈⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⣿⠁⠀⠀⠐⠀⠀⠈⠳⢦⠀⠀⠀⠀⠀⠀⣼⠏⠁⠈⠀⢻⣆⠀⢀⢀⠀⠉⠙⠀⠠⠌⠀⠀⠐⠀⠠⠀⢀⡘⣷⠀⠧⢄⣠⣤⣌⡃⡇⠀⡏⢳⣄⠀⠀⠀⠀⠹⣶⡀⠀⠀⠀⠀⠀⠀⠀
+⠀⠁⣤⡶⠋⢠⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣾⡇⠐⠀⠂⠀⠀⠀⠀⠀⠀⢲⣄⠠⠄⠀⣸⠏⠀⠀⠀⠐⠀⡉⠻⢦⣀⢠⠀⠀⠀⠀⠀⠀⠀⠀⠠⢂⣴⠞⠁⣿⡄⠀⠀⠀⠀⠈⡇⣇⠐⣸⢸⠉⠦⡀⠀⠀⠀⠈⣷⡄⢐⠀⠀⠀⠀⠀
+⠛⠋⠉⠅⠀⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⠀⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⢿⡆⠂⢠⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⡉⠳⠶⣆⣀⣀⣀⡐⣠⣴⡾⠏⠡⠌⠀⢸⡇⠀⠀⠀⠅⠀⡇⢻⡆⣼⢸⠀⢈⣳⠀⠀⠀⠀⡈⢿⡀⠀⡄⠀⠀⠀
+⠈⠁⠀⠑⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣀⣀⣀⡀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢿⠂⡀⠀⡀⠀⠀⠀⠀⠀⠀⡁⠀⢻⣆⢸⣿⠀⠀⠄⠀⠀⠀⠁⠀⠀⠀⠀⠀⠀⠉⠉⠉⠉⠉⠍⠀⠈⠀⠁⠀⢸⡇⠀⠀⠀⠂⠀⢃⠈⣿⠀⢸⠀⠀⠈⣧⠀⠀⠀⠀⠘⢿⡄⠀⢸⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⣀⡀⠀⠂⣀⣴⠶⠟⠛⠛⡉⢉⡉⠩⠍⢙⠒⣤⣀⣀⡀⠀⠀⠀⠀⠈⠀⣧⠄⠀⠀⠀⠀⡀⠀⠀⠀⠐⠀⠻⡆⣿⠀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⡇⠀⠀⠀⠀⠀⠀⠀⣻⠀⣬⠀⠀⠀⢈⣇⠀⠀⠀⠇⠘⣧⠀⢰⠀⠀
+⠀⠀⠀⠀⠀⠤⠀⣠⣤⡾⠟⠋⠡⠀⠁⠀⠀⠀⠀⠀⠀⠀⠳⡆⠄⡏⠀⠌⡙⣓⠶⣤⣄⠰⣦⠀⠂⠀⠀⢀⣴⠾⠛⠛⠛⠶⠦⣝⠉⠨⠙⢷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⢠⣿⠁⠀⠀⠀⠀⠀⠀⠀⠉⠀⠘⣷⡀⢀⡀⠘⠀⠀⠀⠀⠀⢿⡆⢘⠀⠀
+⠀⠀⠀⠀⢀⣤⡾⠟⣁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⡇⡄⠀⠀⠀⠄⢸⣧⠈⠙⢷⣼⣗⠀⢀⠀⡾⠁⠄⠀⠈⠉⠁⠀⠈⢻⣄⠠⠜⣇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠠⠄⢀⣾⠏⢀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⡇⢠⠀⠀⣷⠀⠀⠀⠀⢸⣇⠈⠀⠀
+⠂⠄⣠⣶⢟⡁⢀⠀⠓⡶⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠰⢮⠍⠁⠁⠀⠀⠀⠘⢘⣿⠀⠀⠀⠈⠻⡆⠈⡀⢐⠀⠀⠁⠀⠀⠀⠀⠀⡠⠻⣷⡈⠺⡆⠐⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣾⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡗⠀⠄⡀⢹⡄⠀⠀⠀⠀⣿⠀⠀⠀
+⢠⣾⠋⠀⢸⡇⠄⠀⠀⠠⠈⠷⢠⣀⣀⣀⣀⣀⡤⠞⣁⠀⠀⠀⠀⠀⠀⠀⠼⠘⣿⠁⠀⠀⠀⠀⠀⠐⣇⠀⠀⠀⠀⠀⠀⠀⠀⠐⠀⠐⠈⢿⡆⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡐⣋⣤⣾⠟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠐⠀⠌⢀⣿⠇⢀⠀⠐⢸⠁⠀⠀⠀⠀⣿⡁⠀⠀
+⠟⠁⠈⠀⠀⠩⠀⠦⠀⣀⢀⡀⠀⠀⠅⠈⠉⠠⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡼⠃⠀⠀⠀⠀⠀⠀⠀⠀⢻⠀⠀⠀⣠⣄⡀⠀⡘⠁⠀⠀⠄⠈⢿⡆⠁⠀⠀⠀⠀⠐⠒⢀⣀⣀⣀⠀⠃⠶⠶⢛⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠼⠿⣽⡆⠀⠀⢀⡍⠀⠀⠀⠀⠄⣿⡃⠀⠀
+⠒⡈⢰⣿⠿⠽⣟⣹⠳⢤⣍⠂⠨⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡄⣴⠇⠀⠀⠀⠀⠀⠀⠀⠀⠈⣇⠈⠘⢧⡈⣇⠈⠁⠀⠀⠀⠀⢐⠈⣿⠀⠀⠀⠀⠄⢠⣾⠟⠩⠈⠙⢿⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⠀⠀⣼⡟⠀⡀⢠⠞⠀⠀⠀⠀⠀⠀⣿⠀⠀⠀
+⡉⠖⣠⡋⠛⠛⠋⠀⠐⠀⠙⢷⣌⠠⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢈⣼⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠂⠸⡄⠀⠀⠁⠸⢐⠀⠀⠀⠀⠀⠈⠀⢸⠆⠀⠀⠀⠀⣿⠁⠀⠀⠀⠀⠀⢹⣆⠀⠀⠀⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠶⠚⢉⣄⣞⡱⠃⠀⠀⠀⠀⠀⠀⢸⣿⢀⠀⠀
+⠛⡶⣉⠲⣍⡀⢀⠁⢀⣤⣤⣀⡙⢷⡁⠀⠀⠀⠀⠀⠀⠀⠀⠐⣠⡶⠟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠁⢳⡀⠀⠀⠸⠈⠀⢠⠀⠀⠀⠀⠀⢸⡆⠀⠀⠀⠐⢏⣐⠘⠓⠒⠲⣀⠀⢹⣇⠀⠀⡇⠀⠀⠀⠀⠀⠀⠀⠀⢈⠓⠛⠛⠈⠀⠁⠀⠀⠀⠀⠀⠀⠀⠀⢸⡏⢰⠀⠀
+⢰⠁⠈⠓⢦⣙⠚⢰⡏⠀⠀⠉⠙⡊⠇⣆⣒⠈⠁⣀⣀⣤⣴⠟⠋⠀⠀⠈⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠳⡄⠡⡀⠀⠁⠀⠀⠀⠀⠀⠀⣸⡇⠀⢂⣤⠄⠛⠛⣷⠀⠁⠁⢸⠀⡈⣿⠀⠀⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠀⠀⠀⠀⠀⠀⠀⠀⡄⠀⡾⠁⠈⠀⠀
+⢸⣀⡀⠂⠁⠈⠃⢤⡍⢑⡒⠀⠀⠀⠀⣄⠉⠙⣛⠛⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣴⠶⣦⣤⢤⠀⠂⠀⠀⠙⢦⡀⢶⡀⠀⠀⠀⠀⢈⣠⡟⠀⢀⡞⡀⢸⠙⠀⠈⠳⠀⠀⢸⡇⠀⢹⡄⠀⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⠀⠁⡼⠁⠀⢸⠀⠀
+⢲⣨⣑⡊⠥⣤⣀⢻⡇⠠⠉⠉⠀⠁⠀⠙⢳⣤⣀⠈⠀⠀⠀⠀⠀⠁⣠⣤⡄⠈⠠⣾⠋⢀⠀⠀⠀⠙⠻⣶⡄⠁⠀⠀⡀⠀⠈⠓⠶⠤⠶⠟⠋⠤⠀⠸⡇⠁⢸⠈⠀⠀⠀⠀⠀⣸⠀⠠⢺⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠼⠀⠀⠀⠀⠀⠀
+⢸⡈⠉⠙⠓⠲⠤⠤⢿⣄⠑⠀⠀⠀⠀⠀⠀⠈⠛⠓⠦⢤⣴⣤⣤⣾⣯⡿⠃⡄⣼⡏⠀⠀⠀⠀⠀⠈⠀⠈⠳⡄⣤⣤⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣯⠀⠘⢠⠀⠄⠀⠀⠀⡏⠀⠀⣼⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⠆⠀⠀⠀⢀⠀
+⠘⡆⠀⠀⠀⠤⠀⠀⠄⠙⣷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠬⣉⣁⡠⠶⠃⣿⡇⠄⠀⠀⢁⡐⠊⠙⠱⣱⡶⢋⣌⠉⠻⠶⣌⠀⡀⠀⠀⠀⠀⠀⠀⠐⣆⠀⠈⠂⢦⣤⠤⠞⠀⢀⢀⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⡀⠠⢀⡠⡀⠄⠄⠚⢋⣴⠇⠀⢀⣄⠀
+⠀⢱⠀⠀⠀⠀⠀⠀⠀⢸⣄⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢛⠛⠀⠉⠉⠉⠄⠀⣿⡇⠁⠀⠀⠚⡁⠀⠂⢰⡇⠀⠘⠘⢶⠀⠆⠙⢷⡀⠀⠀⠀⠀⠀⠀⠀⠹⣄⡀⠆⡀⢠⠖⡔⢂⣠⡾⠋⢀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠡⣔⡲⣙⢦⡤⠳⣄⢀⠤⡀⠈⠉⠀⠐⠀⣿⠀
+⠀⠈⢇⠀⠀⠀⠀⠀⠀⠈⠻⣷⣦⣤⣤⠐⡀⠀⠀⠀⠀⠀⠉⠁⠀⠀⠀⠀⠀⠀⠻⡇⠂⠐⠀⠓⡀⠀⢠⠸⣇⠀⠀⠀⢸⠀⠀⠀⣌⠇⠀⠀⠀⠀⠀⠀⠀⠄⣈⠛⡶⣶⣶⢰⡖⠙⠋⠀⠄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⠙⣎⠢⡙⡆⡘⢆⠊⢠⡀⠀⠀⠈⣰⡿⠀
+⠀⢠⠈⡆⠀⠀⠀⠀⠀⠀⠁⣤⠈⠉⠉⢀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢿⡀⠀⡀⠀⢱⣀⠠⠀⠀⠁⠀⣠⠏⠀⡄⠀⣹⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⡶⠂⣀⠂⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢈⡀⠀⢀⣀⡈⢣⠑⡌⢦⢪⡅⡈⢤⣠⠀⢠⣿⣧⠀
+⠀⠀⠀⠀⢦⡀⠀⠀⠀⠀⠀⠠⣘⡛⠛⠋⢁⣀⣤⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠧⡉⠀⠀⠀⠙⠒⠒⠲⠊⠡⢄⠀⢄⠀⣩⠂⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⡟⢠⡾⠋⠔⠀⠀⠀⢀⠀⢀⡤⣶⡋⣯⣍⣽⠟⢁⠆⣻⠀⠙⠈⢦⠱⡜⡘⠄⣁⠀⠀⠀⣾⠀
+⠀⠀⠀⠀⠀⠳⣄⡀⠀⠀⠀⠀⠤⠒⠂⣋⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠻⣦⡀⠀⠀⠄⢀⡄⡀⠸⠬⠉⢴⠾⠉⠀⠀⠀⠀⣀⡀⣀⠀⡀⠀⠀⠀⠀⠀⠀⠘⠑⢈⠀⠀⠠⠀⠀⠀⣠⣤⡿⣻⠲⢖⡛⣩⣤⣬⡴⠞⠻⣄⠀⡄⠐⡇⠙⡄⠀⠀⠀⠀⣼⡟⠀
+⡀⠠⠀⠀⠀⠀⠈⠙⢤⣀⠀⠀⠀⠀⠀⠒⠋⠽⠂⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠹⠿⢷⣶⣶⣶⣶⡬⠭⠉⡀⠀⠀⣄⠀⢠⡀⠄⢱⢀⠙⣦⠀⠀⠀⠀⠀⠀⠀⠀⢀⣶⠆⠁⣰⡿⢿⣩⠶⠋⠉⠉⠉⠀⠀⠀⠂⠀⠀⢀⠈⣧⢹⡄⠀⠀⠙⠀⠀⠠⣾⠋⠀⠈
+⢿⣤⠀⠀⠀⠀⠀⠀⠀⠈⠉⠓⠦⠴⠖⠚⠚⠂⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⠀⠒⠀⠀⠀⠀⠀⠁⠘⢧⡀⠙⣆⠣⡳⡴⡌⢦⡀⠀⠀⠀⠀⠀⠀⠿⠁⣴⣻⣻⠶⠋⠀⠀⠈⠀⠀⠀⠀⠀⠀⠁⠀⠀⠈⠀⠈⠀⠁⠀⠀⠀⠀⠀⢀⣼⠃⠀⠀
+⠨⠉⠳⣶⣀⠀⠂⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢊⣉⣉⣉⣙⠂⣠⠘⢛⡦⠐⠚⣦⡑⢂⡈⢣⡈⠊⠎⣄⠱⡀⠄⠀⠀⠀⠀⠀⠞⠿⠏⢁⠂⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢁⣠⡾⠁⠀⠀⠀
+⠀⠈⠀⢈⣹⣿⠆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠈⡉⠀⢹⡀⣰⡗⢀⠳⣌⢳⡈⢳⡀⠹⣆⠀⢳⡀⠙⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠂⣀⠀⢾⣟⣵⡾⠾⠶⣦⣄
+⠀⠈⠀⠻⢧⣄⣀⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⠀⠈⠛⠋⠑⠀⠀⠈⢦⠁⣂⣃⠀⠈⠃⠀⠁⠀⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⣠⣴⠮⣯⣄⣀⠲⠠⡀⠉
+⠀⠀⠀⠀⠀⠀⠉⢉⣉⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠤⠀⠀⠄⠀⠀⠀⢀⣴⠰⠶⠶⠀⠀⠀⠀⣀⣠⣀⡀⠀⠁⠀⠀⠈⠁⢠⡴⡼⡿⠀⠀⠀⠀⠀⠀⠠⠄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠤⠀⠀⠀⣻⠟⢋⣉⠀⣀⡤⠈⠻⢦⡁⠁⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠛⣃⣀⡀⠀⠀⠀⠀⠀⣀⠀⠀⠀⠀⠀⣀⣠⣤⣶⡶⠶⠾⣚⣲⣒⣶⣤⣤⣤⡤⣤⣄⠀⠄⢠⠏⣁⣉⡙⣦⠀⠀⠀⠀⠀⠌⠛⠛⢃⣀⣤⣤⢤⢦⣤⣦⣤⣤⣄⣄⣀⡀⠀⠀⠀⠀⠀⠀⠒⠀⠐⠀⠀⠚⠛⠃⠞⠉⠈⠀⢈⠻⣄⡀⠀⠀⠈⠛⢠⣔
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠀⠉⠩⠁⡌⣅⠂⠲⣅⠈⠳⢄⠂⠀⣞⣏⣍⣧⠶⢖⣛⣿⠿⠟⠋⠉⠉⠉⠀⠉⠀⠀⠀⠀⣾⢀⣿⠀⢸⠘⡇⠀⠀⠀⠀⠐⣠⡴⠿⠛⠉⠀⠀⠀⠀⠀⠀⠀⡄⠀⠛⠛⠳⠶⠄⠐⠖⠿⠛⠻⠟⢛⡛⡛⠀⠈⠀⠀⠀⠀⠀⢀⠈⢷⡀⠀⠀⠀⠀⢉
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⡶⠂⠃⠀⢈⡳⣄⠙⢓⣄⡀⡄⠞⡓⠂⣡⠔⠛⠉⠡⠀⠀⠀⠄⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⢀⡏⡆⠼⠇⢹⣄⠀⢡⣤⡾⠋⠠⠀⠀⠀⠀⠀⠀⠀⠀⢈⣠⣥⣤⣄⡀⠀⠀⠀⠀⠀⠀⠂⠈⠛⠮⠇⠻⢦⡐⠂⠀⠀⠀⠀⠀⠀⡈⢷⡀⠄⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠈⠀⣿⠀⠠⡀⠀⠳⣔⠮⠳⣄⠰⠙⢦⠲⠒⠛⠹⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡄⠧⡏⠑⠀⠂⠉⡋⢸⡏⠀⠀⠀⠀⠀⠀⠀⣠⡄⡀⠀⠀⠀⡀⠨⠉⠙⠻⠶⠄⠀⠀⠀⠀⠀⠀⠀⡀⢀⠈⣿⡄⠀⠀⠀⠀⠀⠀⠋⢸⡆⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠁⠀⠙⢷⣤⠀⠠⢦⠌⠧⡕⠈⠳⣌⠨⠑⠈⠳⣀⠘⢦⢠⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣇⢰⠌⠀⠀⠂⠀⡇⢼⠀⠀⠀⠀⢀⠀⣠⡾⢁⠀⠃⠀⠀⠀⠀⠀⠀⠀⠙⠑⠋⠀⠀⠀⠀⠀⠀⠀⡁⠀⢀⠌⢻⣌⠀⠀⠀⠀⠀⠐⣰⡇⠂⢠⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣽⠇⠀⢚⡀⠀⠙⢦⠑⢈⡓⢈⢳⣄⢨⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡀⠀⢛⠠⠦⠌⠀⠀⣘⢁⢈⠀⠀⠀⢀⠜⢸⡟⠀⠈⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠊⡀⠙⣮⠡⠄⠁⢀⣰⠟⠀⠂⢀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⠶⣶⠦⠄⠀⠈⢰⠀⠀⠈⠀⠠⠙⠀⠀⢀⣀⣠⣤⣤⣴⣴⣶⡶⣶⣶⣶⣦⣤⣤⡄⠘⠋⠠⠀⠞⠛⠛⠛⠛⠙⠛⠰⢶⣤⡁⣿⠀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠠⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠠⠀⠀⠀⠀⠈⠛⠒⠒⠛⠁⠀⠀⠀⠈⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣤⣤⡅⠈⠀⠀⢀⣠⣴⠶⠞⠛⡏⣉⠉⠉⡉⡉⠈⠉⣴⠟⢁⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠁⠀⠀⠀⡈⡉⠿⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠄⢀⣠⣤⣶⡶⠔⡆⠀⠀⠀⠀⠀⠀⠀⠀⠈⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⠀⠄⠀⣰⠾⠋⢡⡏⠀⠀⢸⠇⡄⠀⠀⠀⠀⠀⠄⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠠⠀⠀⠀⠀⠀⠀⠀⠀⢀⠀⠀⠀⠿⢻⡄⠀⠠⠀⠀⠀⠀⠀⠀⣠⡾⠛⠉⠁⠐⠶⠖⠃⠀⠀⠀⠈⠀⠀⠀⠀⠀⠀⢀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣶⠟⢋⣤⠖⠊⠒⠀⠄⣾⠀⠀⠀⢸⠀⠃⠀⠀⠀⠀⠀⢈⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠐⠀⠀⠀⠀⠠⡆⢶⣶⣦⣤⣤⣤⣄⣻⡄⠀⠀⠀⠀⠀⣀⠀⠋⠀⠀⠀⠀⠠⠀⠀⠀⠀⠀⠄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠻⣷⡀⡼⠁⠂⠀⠀⠀⠀⡿⠀⠀⠀⣿⠀⠀⠀⠀⠀⠀⠀⢸⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠓⠒⠂⠀⠈⠁⠀⠉⠙⠛⠻⣦⡀⢒⠎⣟⠀⠆⠀⠀⠀⠄⠠⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⣤⡞⢃⡅⠀⠀⠀⠀⠰⢼⠀⠰⠀⠀⢹⠀⡀⠀⠀⠀⠀⠀⣼⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠐⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢻⡄⢠⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠁⠀⠀⠉⠉⠉⠀⡞⡇⠀⠀⠀⠰⠀⣼⠃⠀⠀⢀⠘⣧⡉⠀⠀⠀⢂⣴⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢿⣾⠁⠀⠄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠐⠂⠀⢧⠀⠠⠄⢀⣠⡾⠃⠀⠀⠀⠀⠀⠈⠳⠶⠶⠿⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣼⣇⠀⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣁⣠⡾
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠠⠈⠷⠶⠶⠿⠋⡀⠀⠀⠀⠀⠀⠀⠐⠀⠀⠀⠀⠄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⡔⣀⣀⠀⠀⠀⡀⠀⠀⠀⠀⠀⣘⠇⠙⠳⠀⠀⣀⠀⡀⢀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠠⠖⠛⠉⣋⣐⣉
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡀⠄⠂⠀⠰⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠂⠀⠀⠀⠘⠧⠙⠻⠿⣤⣀⡀⠐⣀⣀⡀⣸⡟⠀⠀⠠⠜⣰⢀⠲⠦⣤⣉⣙⡒⠲⠦⠶⠤⡤⠶⠖⡖⢔⡒⠀⠠⣄⣨⣉⣀⣀⣻⣏⢋
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⠀⠀⠀⠀⠀⠀⠀⠈⠙⢷⣄⠀⢀⣴⠟⠀⣥⣤⣀⡴⠋⠰⠁⠁⠀⠉⠛⠛⠛⢛⠈⢉⠰⠷⣶⠆⠘⠻⠷⣀⠔⠈⢀⠉⠉⠉⠀⡌
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠀⠀⠀⠀⠀⠀⠀⠀⠁⠀⢿⡞⢛⣁⣀⣼⠇⠀⠀⠀⠐⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠀⡂⠀⠒⠚⠥⠉⠀⠲⣶⣌⠀⠉⠑⠂⣊
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠐⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡇⢈⠩⠡⠬⠀⠀⠀⠀⣠⣤⡤⠀⠀⠀⠀⠀⠀⠀⠀⢒⡂⠬⣄⠀⠀⠀⠀⢰⡶⠶⠼⠟⠀⠈⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠁⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⠇⠊⠀⠀⠀⠀⠀⠀⠀⣿⠀⠠⣷⡀⠀⠀⠀⠀⡀⡀⠀⠈⠤⠌⠵⡂⣄⠀⠀⣿⡄⢠⠀⠀⢰⠀⠀
+*作者：LateralCircle83
+*开源地址：https://gitee.com/lateralcircle83/eval/tree/master/%E6%8C%87%E5%8D%97%E9%92%88
+*本插件完全免费，如有侵权请立即举报
+*/
+/// <reference path="d:\LateralCircle83\object/dts/llaids/src/index.d.ts"/> 
+
+ll.registerPlugin(
+    /* name */ "direction",
+    /* introduction */ "方向，实时播报旋转角",
+    /* version */[0, 2, 0, 0],
+    /* otherInformation */ { "作者": "LateralCircle83", "许可证": "GPL-2.0" }
+)
+
+if (!ll.hasExported(`cup`, `checkUpdate`)) {
+    logger.warn(`检查更新失败！辅助插件CheckUpdate未安装。>>前往免费下载：https://www.minebbs.com/resources/checkupdate.4792/`);
+    logger.error("魔法先生不知道去哪了qwq快回来快回来")
+} else {
+    const checkUpdate = ll.import(`cup`, `checkUpdate`)
+    checkUpdate("direction", 5136, [2, 0,1])
+}
+// 导出函数
+ll.export(add_direction, "direction", "add_direction")
+ll.export(del_direction, "direction", "del_direction")
+ll.export(list_direction,"direction", "list_direction")
+ll.export(start,"direction","start_direction")
+ll.export(stoper,"direction","stop_direction")
+var all_ids = {}
+//检查配置文件
+var file_main = File.readFrom(".\\plugins\\direction\\main.json")
+if (!file_main) {
+    let main = {
+        "default_on": true,
+        "default_cursor": {
+            "common": "▨",
+            "pi/4": "|",
+            "east": "东",
+            "west": "西",
+            "south": "南",
+            "north": "北",
+            "Resolution": 8
+        }
+    }
+    File.writeTo(".\\plugins\\direction\\main.json", JSON.stringify(main, null, "\t"))
+    file_main = File.readFrom(".\\plugins\\direction\\main.json")
+}
+var file_player = File.readFrom(".\\plugins\\direction\\players.json")
+if (!file_player) {
+    let player = {}
+    File.writeTo(".\\plugins\\direction\\players.json", JSON.stringify(player, null, "\t"))
+    file_player = File.readFrom(".\\plugins\\direction\\players.json")
+}
+file_main = JSON.parse(file_main); file_player = JSON.parse(file_player)
+//命令
+var cmd = mc.newCommand("direction", "罗盘指针系统", PermType.Any, 0x80, "dir")
+cmd.overload()
+cmd.setCallback((_cmd, origin, _output, _res) => {
+    if (origin.type == 0) {
+        setmenu(origin.player)
+    }
+})
+cmd.setup()
+function setmenu(pl) {
+    pl.sendSimpleForm("罗盘菜单", "选择功能：", ["打开罗盘", "关闭罗盘", "添加一个游标", "删除一个游标", "查看游标", "删除全部游标", "设置"], ["", "", "", "", "", "", ""], (pl, id) => {
+        switch (id) {
+            case 0:
+                file_player[pl.xuid].on_start = true
+                File.writeTo(".\\plugins\\direction\\players.json", JSON.stringify(file_player, null, "\t"))
+                start(pl)
+                break
+            case 1:
+                file_player[pl.xuid].on_start = false
+                File.writeTo(".\\plugins\\direction\\players.json", JSON.stringify(file_player, null, "\t"))
+                stoper(pl.xuid)
+                break
+            case 2:
+                menu_add(pl)
+                break
+            case 3:
+                menu_remove(pl)
+                break
+            case 4:
+                let obj = list_direction(pl), str = ""
+                if (!obj) pl.tell("[direction]没有坐标标记")
+                for (let key in obj) {
+                    str = str + "\n" + key + "[x:" + String(obj[key].x) + ",z:" + String(obj[key].z) + ",dimid:" + String(obj[key].dimid) + "]" + obj[key].cursor
+                }
+                pl.tell(str)
+                break
+            case 5:
+                del_direction(pl, "all")
+                pl.tell("[direction]捏爆")
+                break
+            case 6:
+                conf_menu(pl)
+                break
+            default:
+                return
+        }
+    })
+}
+/*玩家数据结构{
+    "初始字符串array_oir":[],
+    "可替换字符串array_last":[],
+    "Resolution"=8,
+    "Resolut"=3.33333333,
+    "i"=i
+} */
+//开辟罗盘
+function start(pl) {
+    // if (file_player[pl.xuid] == null) {
+    //     file_player[pl.xuid] = { "on_start": file_main["default_on"], "cursor": file_main["default_cursor"], "point_to": null }
+    //     File.writeTo(".\\plugins\\direction\\players.json", JSON.stringify(file_main, null, "/t"))
+    // }
+    //运算
+    let cursor = file_player[pl.xuid].cursor
+    let Resolut = 90 / cursor.Resolution
+    let n = 0, e = parseInt(90 / Resolut), s = parseInt(180 / Resolut), w = parseInt(270 / Resolut), pib4 = parseInt(45 / Resolut), pi3b4 = parseInt(135 / Resolut), pi5b4 = parseInt(225 / Resolut), pi7b4 = parseInt(315 / Resolut)//取出特殊值
+    var i = 1, arry = []
+    while (4 * i * cursor.Resolution / 27 < 1) {//循环找到最佳数组i
+        i++
+    }
+    for (let c = 1; c < 4 * cursor.Resolution + 1; c++) {//列出原数组
+        arry.push(cursor.common)
+    }
+    //替换游标
+    arry[n] = cursor.north; arry[e] = cursor.east; arry[s] = cursor.south; arry[w] = cursor.west; arry[pib4] = cursor["pi/4"]; arry[pi3b4] = cursor["pi/4"]; arry[pi5b4] = cursor["pi/4"]; arry[pi7b4] = cursor["pi/4"]
+    var array = []
+    for (let c = 1; c < 3 * i + 1; c++) {//把原始组重复3i次
+        array.push(...arry)
+    }
+    //写入玩家数据结构
+    pl.setExtraData("direction", {
+        "array_oir": array,
+        "array_last": array,
+        "Resolution": cursor.Resolution,
+        "Resolut": Resolut,
+        "i": i
+    })
+    //第一循环
+    var task1 = setInterval(function () {
+        if(pl==null){clearInterval(task1)}
+        let the_dires = pl.direction.yaw, data = pl.getExtraData("direction")
+        let nc = (the_dires + 180) / data.Resolut, zn = 4 * data.i * data.Resolution
+        var arry = data.array_last.slice(zn + nc - 13, zn + nc + 14)
+        pl.setBossBar((Number(pl.xuid) - 83) / 2, arry.toString().replace(/,/g, " "), 50, 3)
+    }, 50)
+    //第二循环
+    var task2 = setInterval(function () {
+        // log(pl.getExtraData("direction"))
+        if(pl==null){clearInterval(task2)}
+        if (!file_player[pl.xuid].point_to) { return }
+        let point_to = file_player[pl.xuid].point_to
+        for (let key in point_to) {
+            let d = point_to[key], x = d.x, z = d.z, dimid = d.dimid, p = pl.pos, px = p.x, pz = p.z, pdimid = p.dimid
+            if (dimid != pdimid) { continue }
+            let cos = (pz - z) / Math.sqrt((x - px) * (x - px) + (z - pz) * (z - pz)), dir, pldata = pl.getExtraData("direction"), i = 0, array = pldata["array_oir"].slice()
+            if ((x - px) <= 0) { dir = 360 - getcosDeg(cos) } else { dir = getcosDeg(cos) }
+            dir = parseInt(dir / pldata.Resolut)
+            while (array.length >= dir + i * 4 * pldata.Resolution) {
+                array[dir + i * 4 * pldata.Resolution] = d.cursor
+                i++
+            }
+            pldata.array_last = array
+            // log(pldata)
+            pl.setExtraData("direction", pldata)
+        }
+    }, 200)
+    all_ids[pl.xuid] = { "task1": task1, "task2": task2 }
+    // log(all_ids)
+}
+//cos到角度
+function getcosDeg(cos) {
+    var result = Math.acos(cos) / (Math.PI / 180);
+    result = Math.round(result);
+    return result;
+}
+//停止
+function stoper(xuid) {
+    let my = all_ids[xuid]
+    // log(my)
+    clearInterval(my.task1)
+    clearInterval(my.task2)
+}
+//玩家进入
+mc.listen("onJoin", (pl) => {
+    if (!file_player[pl.xuid]) {
+        file_player[pl.xuid] = { "on_start": file_main["default_on"], "cursor": file_main["default_cursor"], "point_to": null }
+        File.writeTo(".\\plugins\\direction\\players.json", JSON.stringify(file_player, null, "\t"))
+    }
+    if (file_player[pl.xuid].on_start) {
+        start(pl)
+    } else { return }
+})
+//玩家退出
+mc.listen("onLeft", (pl) => {
+    stoper(pl.xuid)
+})
+//发送添加坐标表单
+function menu_add(pl) {
+    pl.sendForm(form0(), (pl, id) => {
+        if (!id) { return }
+        if (!id[1]) { pl.tell("[direction]未输入名字"); return }
+        log(id)
+        if (!(id[2] && id[3])) { pl.tell("[direction]未输入正确坐标"); return }
+        var pos = { "x": parseInt(id[2]), "z": parseInt(id[3]), "dimid": parseInt(id[4]) || pl.pos.dimid }
+        add_direction(pl, pos, id[1], id[5])
+    })
+}
+function form0() {
+    var fm = mc.newCustomForm()
+    fm.setTitle("添加一个指向")
+    fm.addLabel("在文本中输入")
+    fm.addInput("name:")
+    fm.addInput("x:", "x坐标")
+    fm.addInput("z:", "z坐标")
+    fm.addInput("维度", "0,1,2,不填为当前维度")
+    fm.addInput("游标", "不填为默认§e+§r", "§e+§r")
+    return fm
+}
+//添加坐标函数
+/**
+ * 
+ * @param {player} pl 玩家对象
+ * @param {pos} pos 坐标对象
+ * @param {string} name 操作名称
+ * @param {string} _color 显示字符
+ * @returns {Boolean}
+ */
+function add_direction(pl, pos, name, _color) {
+    var cursor = _color || "§e+§r"
+    let data = file_player[pl.xuid].point_to
+    if (data == null) {
+        data = {}
+    }
+    if (data[name] != null) { return false }
+    data[name] = { "x": pos.x, "z": pos.z, "dimid": pos.dimid, "cursor": cursor }
+    file_player[pl.xuid].point_to = data
+    File.writeTo(".\\plugins\\direction\\players.json", JSON.stringify(file_player, null, "\t"))
+    reload(pl)
+    return true
+}
+// 玩家删除菜单
+function menu_remove(pl) {
+    //一级菜单
+    var data = file_player[pl.xuid].point_to, form = [], form_img = [], i = 0
+    if (!data) { pl.tell("[direction]不存在标记"); return }
+    for (let key in data) {
+        form[i] = key + "[x:" + String(data[key].x) + ",z:" + String(data[key].z) + ",dimid:" + String(data[key].dimid) + "]" + data[key].cursor
+        form_img[i] = ""
+        i++
+    }
+    pl.sendSimpleForm("罗盘删除菜单", "选择一个你要删除的", form, form_img, (pl, id) => {
+        if (id == null) return
+        let all_key = Object.keys(data), name = all_key[id]
+        del_direction(pl, "one", name)
+    })
+}
+// 删除函数
+/**
+ * 
+ * @param {player} pl 玩家对象
+ * @param {string} way 删除方式
+ * @param {string} _name 操作名称
+ * @returns {Boolean}
+ */
+function del_direction(pl, way, _name) {
+    let data = file_player[pl.xuid].point_to
+    if (way == "all") {
+        data = null
+        file_player[pl.xuid].point_to = data
+        File.writeTo(".\\plugins\\direction\\players.json", JSON.stringify(file_player, null, "\t"))
+        reload(pl)
+        return true
+    }
+    if (!_name) return false
+    delete file_player[pl.xuid].point_to[_name]
+    if (JSON.stringify(file_player[pl.xuid].point_to) == "{}") file_player[pl.xuid].point_to = null
+    File.writeTo(".\\plugins\\direction\\players.json", JSON.stringify(file_player, null, "\t"))
+    reload(pl)
+    return true
+}
+// 获取玩家定位项全部信息
+/**
+ * 
+ * @param {player} pl 玩家对象
+ * @returns {Object} 所有目标
+ */
+function list_direction(pl) {
+    let data = file_player[pl.xuid].point_to
+    return data
+}
+function conf_menu(pl) {
+    if (pl.isOP()) {
+        pl.sendModalForm("direction", "检测到您是op，现在您可以搞默认设置和个性设置", "默认设置", "个性设置", (pl, ida) => {
+            if (ida == null) return
+            pl.sendForm(form1(), (pl, id) => {
+                if (id == null) { return }
+                if (ida) {
+                    file_main = { "default_on": id[1], "default_cursor": { "common": id[2], "pi/4": id[3], "east": id[4], "west": id[5], "south": id[6], "north": id[7], "Resolution": id[8] } }
+                    File.writeTo(".\\plugins\\direction\\main.json", JSON.stringify(file_main, null, "\t"))
+                    return
+                } else {
+                    file_player[pl.xuid] = { "on_start": id[1], "cursor": { "common": id[2], "pi/4": id[3], "east": id[4], "west": id[5], "south": id[6], "north": id[7], "Resolution": id[8] }, "point_to": file_player[pl.xuid].point_to }
+                    File.writeTo(".\\plugins\\direction\\players.json", JSON.stringify(file_player, null, "\t"))
+                    reload(pl)
+                    return
+                }
+            })
+        })
+    }
+    else {
+        pl.sendForm(form1(), (pl, id) => {
+            if (id == null) { return }
+            file_player[pl.xuid] = { "on_start": id[1], "cursor": { "common": id[2], "pi/4": id[3], "east": id[4], "west": id[5], "south": id[6], "north": id[7], "Resolution": id[8] }, "point_to": file_player[pl.xuid].point_to }
+            File.writeTo(".\\plugins\\direction\\players.json", JSON.stringify(file_player, null, "\t"))
+            reload(pl)
+            return
+        })
+    }
+}
+function form1() {
+    var fm = mc.newCustomForm()
+    fm.setTitle("direction默认")
+    fm.addLabel("我不想编程啦啊啊啊啊啊")
+    fm.addSwitch("默认开启", file_main.default_on)
+    fm.addInput("一般游标", "", file_main.default_cursor.common)
+    fm.addInput("45度", "", file_main.default_cursor["pi/4"])
+    fm.addInput("east", "", file_main.default_cursor.east)
+    fm.addInput("west", "", file_main.default_cursor.west)
+    fm.addInput("south", "", file_main.default_cursor.south)
+    fm.addInput("north", "", file_main.default_cursor.north)
+    fm.addSlider("分度值，每90度有多少块", 2, 40, 2, file_main.default_cursor.Resolution)
+    return fm
+}
+function reload(pl) {
+    if (!file_player[pl.xuid].on_start) {
+        return false
+    }
+    stoper(pl.xuid)
+    start(pl)
+    return true
+}
