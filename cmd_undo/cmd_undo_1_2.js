48c5949a45a38160e3e8579cf44a7d553753a378
LateralCircle83 <11099324+lateralcircle83@user.noreply.gitee.com>
2024-04-13 10:55:33 +0000

    update
    Signed-off-by: LateralCircle83 <11099324+lateralcircle83@user.noreply.gitee.com>

---
+++ cmd_undo/cmd_undo_1_2.js
@@ -0,0 +1,377 @@
+/**⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⢀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⡙⠐⠔⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠰⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⠎⠢⡀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠐⠃⠀⠤⠠⠀⠀⠀⠀⠀⠀⠀⢰⠒⢤⡀⠀⠀⠀⠀⠀⣀⠤⠔⠒⠒⠒⠠⢤⡀⠀⠀⠀⠀⠀⠀⠀⡀⠀⠜⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠄⠀⠀⠀⠀⠀⠀⠀⣳⠀⠀⠉⢂⡋⠳⡴⠊⠁⠀⠀⠀⠀⠀⠀⠀⡗⠐⢆⡀⠀⠀⠀⠀⠈⡜⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠠⠀⠀⠀⠀⠀⡠⠴⠐⠈⠉⠑⠆⠦⣤⡀⠈⡇⠀⠈⠦⡀⠀⠀⠀⠀⠀⠀⢰⠀⠀⠀⠹⡄⠀⠀⠀⠀⠀⠀⠈⠁⠄⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⢀⡔⠋⠀⠀⠀⠀⠀⠀⠀⡜⠀⠉⠳⡃⠀⠀⠀⢪⠦⢠⠒⠒⢄⢠⠇⠀⠀⢀⡄⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⡀⠨⡀⢠⠄⠀⠀⠀⠀⠀⡜⠀⠀⣠⢤⡘⢆⠀⠠⢦⢧⢸⣀⡀⠀⢳⠀⠀⠀⢸⡅⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⣰⠁⠀⠬⠂⠁⢣⣀⠀⠀⡼⠁⠀⣾⠀⠀⡨⠦⠴⡠⣀⡘⠋⠀⠙⠀⢈⠇⠀⠀⠀⠙⠒⢤⠀⠀⠀⠠⠄⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⡇⠀⠀⠀⣴⠉⠛⠇⠉⠉⠀⠀⠀⢹⠀⠀⠃⠀⠀⠘⡆⠀⣆⠀⠀⠀⡸⠁⠀⠀⠀⠀⠀⢸⠀⠀⠀⠀⠂⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⢸⡀⠀⠀⠀⠸⣄⠀⠙⢤⣀⣀⣀⠀⠀⠓⠠⣀⡀⣀⡰⠃⠀⠈⠑⡊⠉⠀⠀⠀⠀⠀⠀⠀⣸⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⣇⠀⠀⠀⠠⣅⠁⠀⠀⠈⠉⠉⠀⠀⠀⠀⠀⠉⣁⡤⠀⠀⠀⠘⠑⠀⢀⣀⣤⣤⡀⠀⣰⣓⠒⠦⠚⣉⠉⠳⡄⠀⠀⠀⠀
+⠀⠀⠀⠂⠀⠹⡄⠀⠀⠀⢨⣭⡅⠀⠀⠀⠀⠀⠀⣀⣀⣀⣀⣀⠀⠀⠀⠀⡀⢰⢻⠍⡥⢽⢘⠁⢌⡇⠀⠹⢤⠋⠁⠘⡄⢸⣀⡀⠀⠀
+⠀⠀⠀⢀⠤⠴⠉⢢⡀⠀⠀⠀⠀⠀⠀⠀⢰⣿⠛⢹⠯⣁⠤⠜⡕⠀⢄⠔⣣⠊⠘⠨⣇⠿⠈⠰⠭⠢⠤⠜⠀⠢⠤⠔⡥⠌⠬⡙⣦⠀
+⠀⠀⢰⠁⠀⠀⠀⠀⠉⠓⢒⠶⠆⠀⢀⢆⡀⢹⡄⠈⠆⠹⣶⠅⡃⠀⠀⠐⡂⠀⠀⠑⠒⠁⠀⢰⣦⠊⠁⠉⠉⠉⠢⡸⡀⠀⠀⡇⢘⠀
+⠀⠀⠀⠱⢄⠀⠀⠀⠀⠀⠈⠓⠤⡤⠀⠘⠘⠋⠁⠀⠈⠒⠒⠊⠀⠀⠀⠁⠀⠀⠀⢀⡤⢲⠴⠛⡜⢄⠀⠀⠀⠀⠀⢡⢈⠵⡍⠐⣯⠀
+⠀⠀⠀⠀⢈⠀⠀⠀⢄⠀⠀⠀⠀⠙⢀⢤⣀⢤⠄⠤⠲⢀⡤⢤⡀⠀⡎⡩⡷⡀⠀⠀⠀⠀⠀⠒⢡⡀⠋⢀⠀⠀⠀⠃⣇⠀⢀⠇⡟⠀
+⠀⠀⠀⢀⠞⠀⠀⠪⠊⠀⠀⠀⠀⢀⡨⠁⠀⠀⠀⠀⠀⢸⠀⡖⢌⢦⢇⠡⣡⢳⠀⠀⠀⢀⣀⠀⢄⡃⠀⠀⠑⠂⠚⠀⣈⣉⣍⠜⠁⠀
+⣀⠤⠚⠁⠀⠀⠀⠀⠀⠀⠀⢀⡄⠼⠂⠀⠀⣞⣁⠀⠀⠈⢧⠘⠬⠀⠁⠀⠀⡸⡀⠀⠀⠒⠀⠀⠠⡃⠀⠀⠀⠀⢀⠨⠀⠀⠀⠀⠀⠀
+⠁⠀⠀⣀⡀⢀⠀⠀⢀⡀⠀⠀⠓⢀⠔⠀⠀⠀⠙⠰⠆⠀⠀⢱⠀⢄⠰⠀⠊⠁⢈⢮⠆⠀⠀⠀⠈⡆⠀⠀⢀⡰⠋⠀⠀⠄⠊⠣⡀⠀
+⠀⠀⠠⠛⠁⠀⠑⠄⠁⠀⢡⠀⠀⢓⠔⣀⠀⠀⠀⠀⠀⠀⠀⠘⡆⢀⠀⠒⡆⠊⠉⢉⡛⢦⠀⡔⠪⠀⣀⡐⠋⠤⢄⡀⠀⠀⢡⠊⠀⠀
+⠀⠀⠨⡀⠀⠀⠀⠀⠀⠄⠀⠂⠀⡐⠅⠁⠀⠀⠀⠤⠤⡤⠤⡔⠋⠉⢋⠉⠀⠀⠀⠉⠀⠈⠳⡅⠀⠊⠀⠤⣀⠀⡀⠠⡀⣀⡀⠀⠀⠀
+⠀⠀⠀⢁⠀⠀⠀⠀⠁⠀⠀⠀⢰⠇⠀⠀⠀⠀⠀⣆⠀⠈⣇⠹⡄⠀⠘⠀⠀⠀⠀⠀⢀⣀⡀⣩⡀⠀⠀⠀⠀⠈⠁⠁⠋⢠⠂⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠈⠁⠀⠀⣀⡴⠋⠀⠀⠀⠀⠀⠀⠘⢆⣀⡄⠀⠈⠒⠒⠁⠀⠀⠀⠀⠀⠀⠈⢺⠘⣄⠛⠀⠀⣀⠀⢀⠠⠊⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⢀⡜⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡸⠰⡅⠁⠀⡄⢳⠚⠁⠀⠀⠀⠀⠀⠀
+*作者：LateralCircle83
+*开源地址：https://gitee.com/lateralcircle83/eval/
+*本插件完全免费，如有侵权请立即举报
+*/
+//LiteLoaderScript Dev Helper
+/// <reference path="d:\LateralCircle83\object/dts/llaids/src/index.d.ts"/> 
+
+
+ll.registerPlugin(
+    /* name */ "cmd:undo",
+    /* introduction */ "指令后悔神器",
+    /* version */[1, 2],
+    /* otherInformation */ { "作者": "LateralCircle83", "许可证": "GPL-2.0" }
+)
+
+mc.listen("onServerStarted", () => {
+    var cmd = mc.newCommand("cmd:undo", "命令撤销",PermType.GameMasters)
+    cmd.setAlias("cundo")
+    cmd.setEnum("enum", ["do"])
+    cmd.setEnum("list", ["list", "conf"])
+    cmd.mandatory("act", ParamType.Enum, "enum", 1)
+    cmd.mandatory("act", ParamType.Enum, "list", 1)
+    cmd.mandatory("int", ParamType.Int)
+    cmd.overload(["enum", "int"])
+    cmd.overload(["list"])
+    cmd.setCallback((_cmd, ori, output, res) => {
+        let un
+        switch (res.act) {
+            case "do":
+                var id = res.int
+                var dd = date_of[id]
+                if (dd == null) { output.error("[cmd:undo]找不到这个id"); return }
+                switch (dd.type) {
+                    case "tp":
+                        un = new Undo(dd)
+                        un.tp()
+                        output.success("[cmd:undo]已经undo(除死亡的实体和退出的玩家)")
+                        break
+                    case "fill":
+                        un = new Undo(dd)
+                        un.fil()
+                        output.success("[cmd:undo]已经undo，恢复结构")
+                        break
+                    case "clear":
+                        un = new Undo(dd)
+                        un.clea()
+                        // date_of[id]=date
+                        output.success("[cmd:undo]已经undo，恢复玩家背包")
+                        break
+                    case "kill":
+                        un = new Undo(dd)
+                        un.kil()
+                        output.success("[cmd:undo]已经undo，我不知道怎么说")
+                        break
+                }
+                break
+            case "list":
+                var str = "有如下记录：\n"
+                for (id in date_of) {
+                    str = str + String(id) + ":" + date_of[id].type + "\n"
+                }
+                output.success(str)
+                break
+            case "conf":
+                output.success("以后再说")
+        }
+    })
+    cmd.setup()
+
+    const thetpback = mc.newCommand("test_tp", "虚假的tp", PermType.Console)
+    thetpback.mandatory("destination", ParamType.Actor)
+    thetpback.mandatory("victim", ParamType.Actor)
+    thetpback.mandatory("destination_pos", ParamType.Vec3)
+    thetpback.optional("cfb", ParamType.Bool)
+    thetpback.optional("yR", ParamType.Float)
+    thetpback.optional("xR", ParamType.Float)
+    thetpback.overload(["destination", "cfb"])
+    thetpback.overload(["victim", "destination", "cfb"])
+    thetpback.overload(["destination_pos", "yR", "xR", "cfb"])
+    thetpback.overload(["victim", "destination_pos", "yR", "xR", "cfb"])
+    thetpback.overload(["destination_pos", "cfb"])
+    thetpback.overload(["victim", "destination_pos", "cfb"])
+    thetpback.setCallback((_cmd, _ori, _out, res) => {
+        c_get = res
+        // log(res)
+    })
+    thetpback.setup()
+
+    const thefillback = mc.newCommand("test_fill", "虚假的fill", PermType.GameMasters)
+    thefillback.mandatory("from", ParamType.BlockPos)
+    thefillback.mandatory("to", ParamType.BlockPos)
+    thefillback.overload(["from", "to"])
+    thefillback.setCallback((_cmd, ori, _out, res) => {
+        if (ori.type == 0) {
+            c_get = res
+        }
+    })
+    thefillback.setup()
+
+    const theclearback = mc.newCommand("test_clear", "虚假的clear", PermType.Console)
+    theclearback.optional("player", ParamType.Player)
+    theclearback.optional("itemName", ParamType.Item)
+    theclearback.optional("data", ParamType.Int)
+    theclearback.optional("maxCount", ParamType.Int)
+    theclearback.overload(["player", "itemName", "data", "maxCount"])
+    theclearback.setCallback((_cmd, _ori, _out, res) => {
+        c_get = res
+        // log(res)
+    })
+    theclearback.setup()
+
+
+    const thekillback = mc.newCommand("test_kill", "虚假的kill", PermType.Console)
+    thekillback.optional("entity", ParamType.Actor)
+    thekillback.overload(["entity"])
+    thekillback.setCallback((_cmd, _ori, _out, res) => {
+        c_get = res
+        // log(res)
+    })
+    thekillback.setup()
+})
+class Fanctie {
+    constructor(lite) {
+        this.lite = lite
+    }
+    toPos() {
+        var lst = []
+        for (var en of this.lite) {
+            lst.push(en.pos)
+        }
+        return lst
+    }
+    toSetpos() {
+        var pos_from = this.lite[0]
+        var pos_to = this.lite[1]
+        // 判断最小x
+        let x = Math.min(pos_from.x, pos_to.x)
+        let y = Math.min(pos_from.y, pos_to.y)
+        let z = Math.min(pos_from.z, pos_to.z)
+        return mc.newIntPos(x, y, z, pos_from.dimid)
+    }
+}
+class Undo {
+    constructor(deta) {
+        this.deta = deta
+    }
+    tp() {
+        var vic = this.deta.vic
+        var pos = this.deta.pos
+        for (var i = 0, len = vic.length; i < len; i++) {
+            if (vic[i].uniqueId == null) continue
+            vic[i].teleport(pos[i])
+        }
+    }
+    fil() {
+        var structure = this.deta.structure
+        var pos = this.deta.pos
+        mc.setStructure(structure, pos, 0, 0)
+    }
+    clea() {
+        var date = this.deta.player
+        for (var pluuid in date) {
+            // log(date[pluuid].toString())
+            let plnbt = NBT.parseSNBT(date[pluuid])
+            // delete date[pluuid]
+            // date[pluuid]=plnbt
+            // log(plnbn.toString())
+            /*let plnbt=plnbn
+            plnbt.setTag("Inventory",date[pluuid]["Inventory"])
+            plnbt.setTag("Armor",date[pluuid]["Armor"])*/
+            mc.setPlayerNbtTags(pluuid, plnbt, ["Inventory", "Armor"])
+        }
+        var allpl = mc.getOnlinePlayers()
+        for (var pl of allpl) {
+            pl.refreshItems()
+        }
+    }
+    kil() {
+        // log(this.deta)
+        var player = this.deta.player
+        for (var pluuid in player) {
+            let plnbt = player[pluuid]
+            mc.setPlayerNbt(pluuid, plnbt)
+        }
+        var allpl = mc.getOnlinePlayers()
+        for (var pl of allpl) {
+            pl.refreshItems()
+        }
+    }
+}
+var date_of = {}, c_get
+const conf = new JsonConfigFile(".//plugins//cmd_undo//conf.json", `{"tp":true,"kill":true,"clear":true,"fill":true}`)
+// const db = new KVDatabase(".//plugins//cmd_undo//date")
+// if(db.get("id_n")==null) db.set("id_n",-1)
+if (date_of["id_n"] == null) date_of["id_n"] = -1
+
+mc.listen("onPlayerCmd", (pl, cmd) => {
+    /* if (conf.get(cmd.split(" ")[0])==true){
+ 
+     }*/
+    //log(cmd)
+    let cmd_list = cmd.split(" ")
+    // log(cmd_list)
+    if (cmd_list[0] == "tp" || cmd_list[0] == "teleport") {
+        if (conf.get("tp") == true) {
+            var back = test_tp(cmd_list)
+            // log(back)
+            // 判断指令是否可以执行
+            if (!back) return
+            if (((back.destination != null) && (back.destination[0] == null)) || ((back.victim != null) && (back.victim[0] == null))) return
+            // 写入缓存
+            var obj = {}
+            obj["type"] = "tp"
+            if (back.victim == null) { obj["vic"] = [pl]; obj["pos"] = [pl.pos] } else { obj["vic"] = back.victim; let f_p = new Fanctie(back.victim); obj["pos"] = f_p.toPos() }
+            // if(back.destination==null){obj["dest_p"]=back.destination_pos}else{obj["dest_p"]=back.destination[0].pos}
+            // 写入内存
+            let id = date_of.id_n + 1
+            date_of.id_n = id
+            date_of[id] = obj
+            // log(date_of)
+            pl.tell("[cmd:undo]已记录tp指令为id：" + String(id))
+        }
+    }
+    if (cmd_list[0] == "fill") {
+        if (conf.get("fill") == true) {
+            let cmd_lisst = cmd.split("")
+            var back = test_fill(cmd_lisst, pl)
+            if (back == null) return
+            var obj = {}
+            obj["type"] = "fill"
+            let get_stru = mc.getStructure(back.from, back.to, false, false)
+            let f_d = new Fanctie([back.from, back.to])
+            obj["structure"] = get_stru; obj["pos"] = f_d.toSetpos()
+            let id = date_of.id_n + 1
+            date_of.id_n = id
+            date_of[id] = obj
+            pl.tell("[cmd:undo]已记录fill指令为id" + String(id))
+        }
+    }
+    if (cmd_list[0] == "clear") {
+        if (conf.get("clear") == true) {
+            var back = test_clear(cmd)
+            if (back == null) return
+            if ((back.player != null) && (back.player[0] == null)) return
+            var obj = {}, player = {}
+            if (!back.player) back.player = [pl]
+            obj["type"] = "clear"
+            for (var player_o of back.player) {
+                let uuid = player_o.uuid
+                let plnbt = player_o.getNbt()
+                // let Inventory=plnbt.getTag("Inventory")
+                // let Armor=plnbt.getTag("Armor")
+                player[uuid] = plnbt.toSNBT()
+                // player[uuid]={"Inventory":Inventory,"Armor":Armor}
+            }
+            obj["player"] = player
+            let id = date_of.id_n + 1
+            date_of.id_n = id
+            date_of[id] = obj
+            pl.tell("[cmd:undo]已记录clear指令为id" + String(id))
+        }
+    }
+    if (cmd_list[0] == "kill") {
+        if (conf.get("kill") == true) {
+            var back = test_kill(cmd)
+            if (back == null) return
+            if ((back.entity != null) && (back.entity[0] == null)) return
+            var obj = {}, player = {}
+            obj["type"] = "kill"
+            if (!back.entity) {
+                var uuid = pl.uuid
+                let plnbt = pl.getNbt()
+                player[uuid] = plnbt
+                obj["player"] = player
+                let id = date_of.id_n + 1
+                date_of.id_n = id
+                date_of[id] = obj
+                pl.tell("[cmd:undo]已记录kill指令为id" + String(id))
+            } else {
+                for (var xdvb of back.entity) {
+                    if (xdvb.isPlayer()) {
+                        var pla = xdvb.toPlayer()
+                        var uuid = pla.uuid
+                        let plnbt = pla.getNbt()
+                        player[uuid] = plnbt.toSNBT()
+                        continue
+                    }
+                }
+                obj["player"] = player
+                let id = date_of.id_n + 1
+                date_of.id_n = id
+                date_of[id] = obj
+                pla.tell("[cmd:undo]已记录kill指令为id" + String(id))
+            }
+
+        }
+    }
+})
+
+function test_tp(list) {
+    var str = "test_tp"
+    for (var i = 1, len = list.length; i < len; i++) {
+        if (list[i] == "facing") break
+        str = str + " " + list[i]
+    }
+    let ba = mc.runcmdEx(str)
+    if (ba.output.split(" ")[0] == "Syntax") {
+        return null
+    } else { return c_get }
+}
+
+function test_fill(list, pl) {
+    var str = "test_fill"
+    for (var i = 4, len = list.length; i < len; i++) {
+        if (list[i] == " ") { str = str + list[i]; continue }
+        if (list[i] == "~") { str = str + list[i]; continue }
+        if (list[i] == "-") { str = str + list[i]; continue }
+        if (list[i] == "0") { str = str + list[i]; continue }
+        let num = Number(list[i])
+        if (num) { str = str + list[i] } else { break }
+        // log(num)
+        /*if (num == null) {
+            if (list[i] == "-" || list[i] == "~") { str = str + list[i] } else { break }
+            continue
+        }*/
+        /*if (num == 0) {
+            str = str + list[i]
+            continue
+        }*/
+
+    }
+    pl.runcmd(str)
+    let ba = mc.runcmdEx(str)
+    if (ba.output.split(" ")[0] == "Syntax") {
+        return null
+    } else { return c_get }
+}
+
+function test_clear(cmd) {
+    let ba = mc.runcmdEx("test_" + cmd)
+    if (ba.output.split(" ")[0] == "Syntax") {
+        return null
+    } else { return c_get }
+}
+
+function test_kill(cmd) {
+    let ba = mc.runcmdEx("test_" + cmd)
+    if (ba.output.split(" ")[0] == "Syntax") {
+        return null
+    } else { return c_get }
+}
